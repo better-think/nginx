8e8b5845c123362be05568f31c199e1da273bb68
chaojin752 <65781166+chaojin752@users.noreply.github.com>
2021-03-07 15:17:01 +0300

    init

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
 from flask import Flask, session
 from flask_babel import Babel
-from webconfig import APP_SECRET, DEFAULT_LOCALE
+from webconfig import APP_SECRET
 from apps.foods import foods_page
 from apps.users import users_page, login_manager
 from apps import helper
 from apps.helper import current_language
 from db import db
-
-class PrefixMiddleware(object):
-    def __init__(self, app, prefix=''):
-        self.app = app
-        self.prefix = prefix
-    def __call__(self, environ, start_response):
-        if environ['PATH_INFO'].startswith(self.prefix):
-            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
-            environ['SCRIPT_NAME'] = self.prefix
-            return self.app(environ, start_response)
-        else:
-            start_response('404', [('Content-Type', 'text/plain')])
-            return ["This url does not belong to the app.".encode()]
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.secret_key = APP_SECRET
 app.config.from_pyfile('webconfig.py')
 app.register_blueprint(foods_page)
 app.register_blueprint(users_page)
-# app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/recipe')
 app.jinja_env.filters['trans_val'] = helper.trans_val
 app.jinja_env.filters['format_number'] = helper.format_number
 app.jinja_env.filters['format_date'] = helper.format_date
--- webconfig.py
+++ webconfig.py
@@ -5,5 +5,5 @@ UPLOAD_FOLDER = 'uploads'
 APP_SECRET = 'iS37^@z8K6dWwmuWQY'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = get_config_value('database', 'uri')
-SQLALCHEMY_DATABASE_URI = get_config_value('database', 'uri')
-APPLICATION_ROOT = "/recipe"
+DEFAULT_LOCALE='en'
+APPLICATION_ROOT="/recipe"
